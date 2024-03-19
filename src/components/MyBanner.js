3e50a04ada5a8991c4ba8454bfaa940a634a4e47
QGonz95 <q.gonz95@gmail.com>
2024-03-19 14:20:35 -0400

    k'

--- src/components/MyBanner.js
+++ src/components/MyBanner.js
@@ -17,7 +17,7 @@ const MyBanner = () => (
     <Stack>
       <a href="/exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#FF2625', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
     </Stack>
-    <Typography fontWeight={600} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '175px' }}>
+    <Typography fontWeight={500} color="#FF2625" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '175px' }}>
       Work Hard <br/>Train Hard
     </Typography>
     <img src={Me} alt="hero-banner" className="hero-banner-img" />
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -7,7 +7,6 @@ const Home = () => {
   return (
     <Box>
       <MyBanner />
-
     </Box>
   );
 };
